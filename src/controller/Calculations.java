102bcaf294348b1287e7b38a3c3b5fefca82026e
Julia-VanLandingham <julia.vanlandingham@otterbein.edu>
2021-02-04 22:18:10 -0500

    unit conversions

--- src/controller/Calculations.java
+++ src/controller/Calculations.java
@@ -20,7 +20,23 @@ public class Calculations {
      * @return the length value converted to the ending unit system
      */
     public static double convertLength(Units startingUnits, Units endingUnits, double value){
-        return 0.0;
+        switch (startingUnits){
+            case ENGLISH:
+                switch (endingUnits){
+                    case METRIC:
+                        return value / 39.37;//inches to meters
+                    case ENGLISH:
+                        return value;
+                }
+            case METRIC:
+                switch (endingUnits){
+                    case METRIC:
+                        return value;
+                    case ENGLISH:
+                        return value * 39.37;//meters to inches
+                }
+        }
+        throw new IllegalArgumentException("Illegal units");
     }
 
     /**
@@ -33,7 +49,23 @@ public class Calculations {
      * @return the force value converted to the ending unit system
      */
     public static double convertForce(Units startingUnits, Units endingUnits, double value){
-        return 0.0;
+        switch (startingUnits){
+            case ENGLISH:
+                switch (endingUnits){
+                    case METRIC:
+                        return value * 4.4482216;//pounds to newtons
+                    case ENGLISH:
+                        return value;
+                }
+            case METRIC:
+                switch (endingUnits){
+                    case METRIC:
+                        return value;
+                    case ENGLISH:
+                        return value / 4.4482216;//newtons to pounds
+                }
+        }
+        throw new IllegalArgumentException("Illegal units");
     }
 
     /**
@@ -46,26 +78,65 @@ public class Calculations {
      * @return the pressure value converted to the ending unit system
      */
     public static double convertPressure(Units startingUnits, Units endingUnits, double value){
-        return 0.0;
+        switch (startingUnits){
+            case ENGLISH:
+                switch (endingUnits){
+                    case METRIC:
+                        return value * 6894.76;//psi to pa
+                    case ENGLISH:
+                        return value;
+                }
+            case METRIC:
+                switch (endingUnits){
+                    case METRIC:
+                        return value;
+                    case ENGLISH:
+                        return value / 6894.76;//pa to psi
+                }
+        }
+        throw new IllegalArgumentException("Illegal units");
     }
 
+    /**
+     * Calculate strain
+     * @param elongation elongation measured
+     * @param initial initial value of WHAT????
+     * @return strain
+     */
     public static double calculateStrain(double elongation, double initial){
         return elongation/initial;
     }
 
+    /**
+     * Calculate stress
+     * @param force force measured
+     * @param area area calculated
+     * @return stress
+     */
     public static double calculateStress(double force, double area){
         return force/area;
     }
 
-    public static double calculateArea(double width, double height){
+    /**
+     * Calculate the area of the cross section for a rectangular piece
+     * @param width width of the cross section
+     * @param depth depth of the cross section
+     * @return area
+     */
+    public static double calculateArea(double width, double depth){
         if (width <= EPSILON){
             throw new IllegalArgumentException("Width cannot be zero");
-        }else if(height <= EPSILON){
-            throw new IllegalArgumentException("Height cannot be zero");
+        }else if(depth <= EPSILON){
+            throw new IllegalArgumentException("Depth cannot be zero");
         }
-        return width*height;
+        return width*depth;
     }
 
+    /**
+     * Calculate the area of the cross section for a circular piece
+     * @param diameter diameter of the cross section
+     * @return area
+     */
     public static double calculateArea(double diameter){
         if (diameter <= EPSILON){
             throw new IllegalArgumentException("Diameter cannot be zero");
