8bd325768fb874b7b2a260183158fa3cf383080e
dosch1 <dosch1@otterbein.edu>
2021-02-24 16:01:40 -0500

    Added action listener for main window

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -10,12 +10,15 @@ public class MainController {
 
     private MainWindow mainWindow;
     private InputController inputController;
+    private SettingsController settingsController;
 
     public MainController(){
         mainWindow = new MainWindow();
         inputController = new InputController();
+        settingsController = new SettingsController();
 
         mainWindow.getInput().addActionListener(e -> inputController.getInputWindow().setVisible(true));
+        mainWindow.getSettings().addActionListener(e -> settingsController.getSettingsWindow().setVisible(true));
     }
 
     public static void main(String[] args){
--- src/view/SettingsView.java
+++ src/view/SettingsView.java
@@ -5,34 +5,26 @@ import java.awt.*;
 import java.util.ArrayList;
 import javax.swing.SpringLayout;
 
-
 public class SettingsView extends JFrame{
 
     private final int frameHeight;
     private final int frameWidth;
     private static final String SAMPLE_RATE_STRING = "Default Sample Rate: ";
     private static final String UNIT_SELECTION_STRING = "Default Units : ";
-    private static final String DEFAULT_ELONGATION = "Default Elongation: ";
+    private static final String DEFAULT_ELONGATION = "Default Elongation (in): ";
     private static final int DEFAULT_SAMPLE_RATE = 100;
     private JTextField defaultElongationField;
 
-
-
     private final String [] measurements = {"psi", "pascals", "Kipp"};
-
 
     private JButton saveButton = new JButton("Save");
     private JSpinner sampleRateSelection  = new JSpinner(new SpinnerNumberModel(1,1,10,1));
     private JComboBox<String> unitSelection = new JComboBox<>(measurements);
     private JLabel unitSelectionLabel = new JLabel(UNIT_SELECTION_STRING);
 
-
-
-
-
     public SettingsView () {
         setTitle("Settings");
-        setResizable(true);
+        setResizable(false);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frameHeight = (int) (screenSize.getHeight() * .95);
         frameWidth = (int) (screenSize.getWidth() * .95);
@@ -42,7 +34,7 @@ public class SettingsView extends JFrame{
         add(createSouthPanel(), BorderLayout.SOUTH);
 
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setVisible(true);
+        setVisible(false);
         pack();
         setLocationRelativeTo(null);
     }
@@ -74,7 +66,6 @@ public class SettingsView extends JFrame{
         return southPanel;
     }
 
-
     public ArrayList<String> getInput(){ //for getting all the inputs from the fields before closing
         return null;
     }
@@ -84,8 +75,4 @@ public class SettingsView extends JFrame{
     }
 
     public JButton getSaveButton(){ return saveButton;  }
-
-    public static void main (String [] args){
-        SettingsView viewTest = new SettingsView();
-    }
 }
