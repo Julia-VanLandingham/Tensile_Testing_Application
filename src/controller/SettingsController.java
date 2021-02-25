b72a61826ec94f38eb57caa6b1bd008455794de0
Christopher Geidans <cgeidans@gmail.com>
2021-02-25 11:34:06 -0500

    Refactored Settings view to add scanner to NorthPanel. Settings  changed in settings view will now persist between program closing and launching.

--- src/controller/SettingsController.java
+++ src/controller/SettingsController.java
@@ -2,17 +2,47 @@ package controller;
 
 import view.SettingsView;
 
-public class SettingsController {
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class SettingsController {
+    private static final String CONFIG_FILE = "settings.cfg";
     private SettingsView settingsWindow;
 
 
     public SettingsController(){
-        settingsWindow = new SettingsView();
+        Scanner input = null;
+        try{
+           input =new Scanner(new File(CONFIG_FILE));
+        } catch (FileNotFoundException e) {
+
+        }
+        settingsWindow = new SettingsView(input);
 
         settingsWindow.getSaveButton().addActionListener(e -> settingsWindow.setVisible(false));
-    }
+        if(input != null){
+            input.close();
+        }
+        settingsWindow.getSaveButton().addActionListener(e -> {
+            try {
+                double value = Double.parseDouble(settingsWindow.getDefaultElongation().getText().trim());
+                PrintWriter out = new PrintWriter(new FileOutputStream(CONFIG_FILE));
+                out.println(settingsWindow.getSampleRateSelection().getValue());
+                out.println(settingsWindow.getUnitSelection().getSelectedItem());
+                out.println(value);
+                out.close();
+            } catch (FileNotFoundException exception) {
 
+            }
+            catch (NumberFormatException exception) {
+                JOptionPane.showMessageDialog(settingsWindow,"Default Elongation value is not a properly formatted number."," Bad Elongation Number",JOptionPane.ERROR_MESSAGE);
+            }
+        });
+    }
 
     public SettingsView getSettingsWindow(){
         return settingsWindow;
--- src/view/SettingsView.java
+++ src/view/SettingsView.java
@@ -3,6 +3,9 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 import javax.swing.SpringLayout;
 
 /**
@@ -12,22 +15,22 @@ public class SettingsView extends JFrame{
 
     private static final String SAMPLE_RATE = "Sample Rate: ";
     private static final int DEFAULT_SAMPLE_RATE = 100;
-    private JSpinner sampleRateSelection  = new JSpinner(new SpinnerNumberModel(DEFAULT_SAMPLE_RATE,DEFAULT_SAMPLE_RATE,(DEFAULT_SAMPLE_RATE * 100),1));
+    private JSpinner sampleRateSelection;
 
     private static final String UNIT_SELECTION = "Units: ";
-    private final String [] measurements = {"psi", "pascals", "Kipp"};
-    private JComboBox<String> unitSelection = new JComboBox<>(measurements);
+    private final String [] MEASUREMENTS = {"psi", "pascals", "kip"};
+    private JComboBox<String> unitSelection;
 
     private static final String ELONGATION = "Elongation (in): ";
     private JTextField defaultElongationField;
 
     private JButton saveButton = new JButton("Save");
 
-    public SettingsView () {
+    public SettingsView (Scanner userInput) {
         setTitle("Settings");
         setResizable(false);
 
-        add(createNorthPanel(), BorderLayout.NORTH);
+        add(createNorthPanel(userInput), BorderLayout.NORTH);
         add(createSouthPanel(), BorderLayout.SOUTH);
 
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -40,21 +43,43 @@ public class SettingsView extends JFrame{
      * Creates fields that allow the user to determine the settings
      * that will be used throughout the test. Those settings include sample rate, units, and elongation
      */
-    private JPanel createNorthPanel(){
+    private JPanel createNorthPanel(Scanner userInput){
         JPanel northPanel = new JPanel(new SpringLayout());
         JLabel sampleRateLabel = new JLabel(SAMPLE_RATE,JLabel.TRAILING);
+        JLabel unitsSelectionLabel = new JLabel(UNIT_SELECTION,JLabel.TRAILING);
+        JLabel defaultElongationLabel = new JLabel(ELONGATION,JLabel.TRAILING);
+        boolean readSuceeded = false;
+        if(userInput != null)  {
+            try{
+                int sampleRate = userInput.nextInt();
+                String selectedUnitType = userInput.next();
+                double elongation = userInput.nextDouble();
+                sampleRateSelection = new JSpinner(new SpinnerNumberModel(sampleRate,DEFAULT_SAMPLE_RATE,(DEFAULT_SAMPLE_RATE * 100),1));
+                defaultElongationField = new JTextField(elongation+"");
+                unitSelection = new JComboBox<>(MEASUREMENTS);
+                unitSelection.setSelectedItem(selectedUnitType);
+                readSuceeded = true;
+            }
+            catch( NoSuchElementException | IllegalStateException e) {
+            }
+        }
+        if(!readSuceeded){
+            sampleRateSelection = new JSpinner(new SpinnerNumberModel(DEFAULT_SAMPLE_RATE,DEFAULT_SAMPLE_RATE,(DEFAULT_SAMPLE_RATE * 100),1));
+            defaultElongationField = new JTextField("0.5");
+            unitSelection = new JComboBox<>(MEASUREMENTS);
+        }
         northPanel.add(sampleRateLabel);
         sampleRateLabel.setLabelFor(sampleRateSelection);
         northPanel.add(sampleRateSelection);
 
-        JLabel unitsSelectionLabel = new JLabel(UNIT_SELECTION,JLabel.TRAILING);
+
         northPanel.add(unitsSelectionLabel);
         unitsSelectionLabel.setLabelFor(unitSelection);
         northPanel.add(unitSelection);
 
-        JLabel defaultElongationLabel = new JLabel(ELONGATION,JLabel.TRAILING);
+
         northPanel.add(defaultElongationLabel);
-        defaultElongationField = new JTextField("0.5");
+
         defaultElongationLabel.setLabelFor(defaultElongationField);
         northPanel.add(defaultElongationField);
         SpringUtilities.makeCompactGrid(northPanel,3,2,6,6,6,6);
@@ -81,4 +106,9 @@ public class SettingsView extends JFrame{
     }
 
     public JButton getSaveButton(){ return saveButton;  }
+
+    public JSpinner getSampleRateSelection(){return sampleRateSelection; }
+    public JComboBox<String> getUnitSelection(){return unitSelection; }
+    public JTextField getDefaultElongation(){return defaultElongationField;}
+
 }
