4f85be27245fde9d59200776ff3dccefc40fb436
Julia-VanLandingham <julia.vanlandingham@otterbein.edu>
2021-03-03 14:52:58 -0500

    add system look and feel and make buttons not focusable

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -2,6 +2,8 @@ package controller;
 
 import view.MainWindow;
 
+import javax.swing.*;
+
 /**
  * Root of the entire program
  * NOTE: This should be the only main method ever actually run
@@ -13,12 +15,27 @@ public class MainController {
     private SettingsController settingsController;
 
     public MainController(){
+        setLookAndFeel();
         mainWindow = new MainWindow();
         inputController = new InputController();
         settingsController = new SettingsController();
 
         mainWindow.getInput().addActionListener(e -> inputController.getInputWindow().setVisible(true));
         mainWindow.getSettings().addActionListener(e -> settingsController.getSettingsWindow().setVisible(true));
+    }
+
+    /*
+     * Set the look and feel for all the windows
+     */
+    private void setLookAndFeel(){
+        try {
+            //Set system look and feel
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args){
